c1e1e40a541255b906b9abd85579b3be306ab04a
Necole Ian Reducto <necoleianreducto@gmail.com>
2018-07-30 13:57:59 +0800

    Sample
    Sample

--- src/test.java
+++ src/test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String Arg[]) {
-        System.out.println("Hello World");
+        System.out.println("Hello World, This is just a test");
     }
 }
